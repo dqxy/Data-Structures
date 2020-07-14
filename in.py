e38d2cdc4eeda592558e97e839cdbebd05ca5d38
dqxy <yanofsky.david@gmail.com>
2020-07-14 15:03:15 -0400

    Python

---
+++ in.py
@@ -0,0 +1,41 @@
+# Print out each element of the following array on a separate line:
+
+#array = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
+
+
+#for x in array:
+#    print(x)
+
+
+# You may use whatever programming language you'd like.
+
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+
+# Print out each element of the following array on a separate line, but this time the input array can contain arrays nested to an arbitrarily deep level:
+
+
+
+#a = [person for person in array]
+#print str(a).strip('[]')
+#print('\n'.join(map(str, mylist[0])))
+
+
+x = ['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
+
+print(x[0])
+print(x[1])
+print(x[2][0])
+print(x[2][1])
+print(x[2][2])
+print(x[2][3][0])
+print(x[2][3][1])
+print(x[2][3][2])
+print(x[2][3][3])
+print(x[3])
+print(x[4])
+print(x[5])
+
+
+
+
--- queue/queue.py
+++ queue/queue.py
@@ -10,19 +10,56 @@ return elements in First In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
    
+    Arrays are index based data structure where each element associated with an index. Linked list relies on references where each node consists of the data and the references to the previous and next element. Linked list points to head and tail.
+
+    Stack: Last item put on is the first item out. LIFO. Append or take off the end. Only the end matters.
+    Que: First person in line gets served first FIFO. Last in line is last served. LILO. Append or take off from the front. Middle does not matter.
+   
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self, init_list=[]):
+#         self.size = 0
+#         self.storage = init_list
+    
+#     def __len__(self):
+#         length = len(self.storage)
+#         self.size = length
+#         return length
+
+#     def enqueue(self, value):
+#         self.storage.insert(0,value)
+
+#     def dequeue(self):
+#         if len(self) > 0:
+#             return self.storage.pop()
+#         else:
+#             return None
+
+import sys
+sys.path.append('C:\\Users\\spira\\Documents\\Data-Structures\\singly_linked_list')
+from singly_linked_list import LinkedList, Node
+
 class Queue:
-    def __init__(self):
+    def __init__(self, init_node=None):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList(init_node)
     
     def __len__(self):
-        pass
+        length = 0
+        if self.storage.head:
+            length = 1
+            ongoing = self.storage.head
+            while ongoing.next:
+                length += 1
+                ongoing = ongoing.next
+        self.size = length
+        return length
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        value = self.storage.remove_head()
+        return value
